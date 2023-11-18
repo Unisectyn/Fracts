c805ba6ccb65a739b10c43a4e99a02b0472084fa
unisectyn <mohamedeball@outlook.com>
2023-11-18 16:58:06 +0100

    implemented drawing the sierpinski triangle based on the odd elements on the rows of pascal's triangle

--- src/main.py
+++ src/main.py
@@ -4,16 +4,29 @@ import shapes_siy as ss
 
 pygame.init()
 
+WHITE = pygame.colordict.THECOLORS["white"]
+
 resolution = (1280,720)
 screen = pygame.display.set_mode(resolution)
 
 clock = pygame.time.Clock()
-for (v_1, v_2, v_3) in ss.sierpinski_triangles(np.array([0, 0]), resolution[1], resolution[0], 10):
+
+# example of usage for each function
+""" for (v_1, v_2, v_3) in ss.sierpinski_triangles(np.array([0, 0]), resolution[1], resolution[0], 10):
     pygame.draw.line(screen, (255, 255, 255), v_1, v_2, 1)
     pygame.draw.line(screen, (255, 255, 255), v_2, v_3, 1)
-    pygame.draw.line(screen, (255, 255, 255), v_3, v_1, 1)
+    pygame.draw.line(screen, (255, 255, 255), v_3, v_1, 1) """
 
-# ss.sierpinski_triangles_draw(screen, np.array([0, 0]), resolution[1], resolution[0], 13)
+""" ss.sierpinski_triangles_draw(screen, np.array([0, 0]), resolution[1], resolution[0], 13) """
+
+# px_arr = pygame.PixelArray(screen)
+# for l, row in ss.sierpinski_triangles_pasc(np.array([0, 0]), resolution[1], resolution[0], 500):
+#     for i in l:
+#         px_arr[i + (row >> 1), resolution[1] - row] = WHITE
+#         px_arr[i + 1 + (row >> 1), resolution[1] - row] = WHITE
+#
+# px_arr.close()
+
 
 while True:
     for event in pygame.event.get():
--- src/shapes_siy.py
+++ src/shapes_siy.py
@@ -31,6 +31,21 @@ def sierpinski_triangles(origin: np.ndarray, height, width, depth):
                      sierpinski_triangles(origin + np.array([0, height / 2]), height / 2, width / 2, depth - 1),
                      sierpinski_triangles(origin + np.array([width / 2, height /2]), height / 2, width / 2, depth - 1))
 
+
+def sierpinski_triangles_pasc(origin: np.ndarray, height, width, depth):
+    row_n_1 = [1]
+    row_n = [1]
+    
+    for i in range(depth):
+        # Send the indices of odd elements
+        yield list(filter(lambda x: x != 0,
+                      map(lambda i_b: (i_b[0]+1) * i_b[1],
+                      enumerate(
+                      map(lambda x: x % 2, row_n))))), (depth - i)
+        
+        # make the next row
+        row_n_1 = row_n[:]
+        row_n = [1] + list(map(lambda p_n: p_n[0] + p_n[1], zip(row_n_1, row_n_1[1:]))) + [1]
 # WIP
 def sierpinski_triangles_iter(origin: np.ndarray, height, width, depth):
     while False:
